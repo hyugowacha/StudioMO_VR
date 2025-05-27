b135d07f5b15ca00c64fa8a81b34d693e2073e31
jungyounghan <superbrush@naver.com>
2025-05-27 10:08:53 +0900

    [Add] 커스텀 XR Interaction 프리팹 제작

---
+++ Assets/1.Scripts/MonoBehaviourPunCallbacks/Managers/StageManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class StageManager : Manager
+{
+    protected override void ChangeText()
+    {
+    }
+
+    protected override void OnLeftFunction(InputAction.CallbackContext callbackContext)
+    {
+        Debug.Log("¿ÞÂÊ");
+    }
+
+    protected override void OnRightFunction(InputAction.CallbackContext callbackContext)
+    {
+        Debug.Log("¿À¸¥ÂÊ");
+    }
+}
