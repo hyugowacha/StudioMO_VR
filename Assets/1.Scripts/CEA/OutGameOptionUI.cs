fdd2252ceafef53858b0e17c4c47ba3b43dd3a62
hyugowacha <uena0506@gmail.com>
2025-06-10 10:50:36 +0900

    [WIP] 아웃게임 UI 작업중

---
+++ Assets/1.Scripts/CEA/OutGameOptionUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutGameOptionUI : MonoBehaviour
+{
+
+    /// <summary>
+    /// 스냅/스무스 변경 함수
+    /// </summary>
+    public void ChangeSnap()
+    {
+
+    }
+
+    /// <summary>
+    /// 왼손/오른손 변경 함수
+    /// </summary>
+    public void ChangeHand()
+    {
+
+    }
+
+
+}
