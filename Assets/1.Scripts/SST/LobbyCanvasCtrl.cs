b014b50ac40f1a8de4d04f32c76ea479e4f72939
Sonseongtae <sonseongtae@gmail.com>
2025-06-12 17:53:13 +0900

    [WIP] 상점 UI 프리팹 수정중
    프리팹 언팩해서 한 캔버스안에 각각 패널로 합쳐놓음

--- Assets/1.Scripts/SST/LobbyCanvasCtrl.cs
+++ Assets/1.Scripts/SST/LobbyCanvasCtrl.cs
@@ -6,11 +6,14 @@ using UnityEngine.UI;
 public class LobbyCanvasCtrl : MonoBehaviour
 {
     [Header("로비 패널들 총괄하는 캔버스")]
-    [SerializeField] private Canvas lobbyCanvas;
-    [SerializeField] private GameObject lobbyPanel;
+    [SerializeField] private Canvas lobbyCanvas;                // 로비 캔버스
+    [SerializeField] private GameObject lobbyPanel;             // 로비 패널
 
     [Header("로비에서 버튼 클릭 시 활성화 될 해당 패널들")]
     [SerializeField] private GameObject stageSelectPanel;       // 스테이지 모드 패널
+
+    [Header("상점 패널"), SerializeField]
+    private GameObject shopPanel;                               // 상점 패널
 
     private void Start()
     {
@@ -18,10 +21,18 @@ public class LobbyCanvasCtrl : MonoBehaviour
         lobbyPanel.SetActive(true);
         stageSelectPanel.SetActive(false);
     }
-
+    
+    // ▼ 스테이지 모드 버튼 클릭 시 호출되는 함수
     public void OnClickStageMode()
     {
         lobbyPanel.SetActive(false);
         stageSelectPanel.SetActive(true);
+    }
+
+    // ▼ 상점 버튼 클릭 시 호출되는 함수
+    public void OnClickShop()
+    {
+        lobbyPanel.SetActive(false);
+        shopPanel.SetActive(true);
     }
 }
