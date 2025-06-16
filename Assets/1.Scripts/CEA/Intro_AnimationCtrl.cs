5eaa557a7e5be5e6429db77396d65574d8a5a31a
hyugowacha <uena0506@gmail.com>
2025-06-16 13:23:03 +0900

    [Add] 인트로 disappear 애니메이션 추가

---
+++ Assets/1.Scripts/CEA/Intro_AnimationCtrl.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Intro_AnimationCtrl : MonoBehaviour
+{
+    [SerializeField] private Animator[] controller;
+    public bool onDisappear = false;
+
+    private void Update()
+    {
+        inspectTrigger();
+    }
+
+    public void inspectTrigger()
+    {
+        if(onDisappear)
+        {
+            foreach (var controller in controller)
+            {
+                controller.SetTrigger("onDisappear");
+                onDisappear = false;
+            }
+        }
+    }
+
+    public void ChangeAnimTrue()
+    {
+        onDisappear = true;
+    }
+
+   
+}
