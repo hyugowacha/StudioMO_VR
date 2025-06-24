ebce03c0deb556d160fe819636d991e1deb8980b
hyugowacha <uena0506@gmail.com>
2025-06-24 18:07:51 +0900

    [Add] Stage1-3 이펙트 포인트 추가

---
+++ Assets/1.Scripts/CEA/MapEffectSpawner.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapEffectSpawner : MonoBehaviour
+{
+    [SerializeField, Header("πŸ¥Ÿ ∏  ¿Ã∆Â∆Æ ∏Ò∑œ")]
+    private GameObject[] sea;
+
+    [SerializeField, Header("∫˘«œ ∏  ¿Ã∆Â∆Æ ∏Ò∑œ")]
+    private GameObject[] glacier;
+
+    [SerializeField, Header("ªÁ∏∑ ∏  ¿Ã∆Â∆Æ ∏Ò∑œ")]
+    private GameObject[] desert;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
