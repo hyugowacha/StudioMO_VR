3e3cf0050e99cd494c48fc2e4c9bd83917b165ad
jungyounghan <superbrush@naver.com>
2025-06-26 16:16:02 +0900

    [Fix] 패널 트래킹 효과 수정

--- Assets/1.Scripts/Panels/PanelLeader.cs
+++ Assets/1.Scripts/Panels/PanelLeader.cs
@@ -8,8 +8,15 @@ public class PanelLeader : MonoBehaviour
 {
     private Dictionary<Panel, Vector3> panels = new Dictionary<Panel, Vector3>();
 
-    [SerializeField, Range(0, int.MaxValue)]
+    [Header("이동 속도"),SerializeField, Range(MinValue, MaxValue)]
     private float speed = 1.0f;
+    [Header("크기 보간비"), SerializeField, Range(MinValue, MaxValue)]
+    private float ratio = 0.007f;
+    //[Header("허용 거리 편차"), SerializeField, Range(MinValue, MaxValue)]
+    //private float distance = 0.0001f;
+
+    private const int MinValue = 0;
+    private const int MaxValue = int.MaxValue;
 
     private void OnEnable()
     {
@@ -21,40 +28,36 @@ public class PanelLeader : MonoBehaviour
         StartCoroutine(MoveToTarget());
         IEnumerator MoveToTarget()
         {
-            Vector3 position = transform.position;
+            Vector3 beforeForward = transform.forward;
             while (true)
             {
-                yield return new WaitWhile(() => position == transform.position);
-                do
+                //yield return new WaitUntil(() => distance < Vector3.Distance(beforeForward.normalized, transform.forward.normalized));
+                //do
                 {
-                    position = Vector3.Lerp(position, transform.position, Time.deltaTime * speed);
-                    Vector3 offset = position - transform.position;
-                    if (offset == Vector3.zero)
+                    Vector3 afterForward = transform.forward;
+                    beforeForward = Vector3.Lerp(beforeForward, afterForward, Time.deltaTime * speed);
+                    Vector3 direction = beforeForward - afterForward;
+                    float x = Vector3.Dot(direction, transform.right.normalized) * ratio;
+                    float y = Vector3.Dot(direction, transform.up.normalized) * ratio;
+                    foreach (KeyValuePair<Panel, Vector3> keyValuePair in this.panels)
                     {
-                        foreach (KeyValuePair<Panel, Vector3> keyValuePair in this.panels)
+                        Panel panel = keyValuePair.Key;
+                        if (panel != null)
                         {
-                            Panel panel = keyValuePair.Key;
-                            if (panel != null)
-                            {
-                                panel.transform.localPosition = keyValuePair.Value;
-                            }
+                            panel.transform.localPosition = new Vector3(x, y, 0) + keyValuePair.Value;
                         }
-                        position = transform.position;
-                        break;
                     }
-                    else
-                    {
-                        foreach (KeyValuePair<Panel, Vector3> keyValuePair in this.panels)
-                        {
-                            Panel panel = keyValuePair.Key;
-                            if (panel != null)
-                            {
-                                panel.transform.localPosition = offset + keyValuePair.Value;
-                            }
-                        }
-                        yield return null;
-                    }
-                } while (position == transform.position);
+                    yield return null;
+                } 
+                //while (distance < Vector3.Distance(beforeForward.normalized, transform.forward.normalized));
+                //foreach (KeyValuePair<Panel, Vector3> keyValuePair in this.panels)
+                //{
+                //    Panel panel = keyValuePair.Key;
+                //    if (panel != null)
+                //    {
+                //        panel.transform.localPosition = keyValuePair.Value;
+                //    }
+                //}
             }
         }
     }
