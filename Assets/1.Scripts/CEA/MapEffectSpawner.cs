5a28ae2fa09aebea2430d6c85efe61f0198f68cc
jungyounghan <superbrush@naver.com>
2025-06-27 14:17:57 +0900

    [WIP] MapEffectSpawner 최적화 속도 개선

--- Assets/1.Scripts/CEA/MapEffectSpawner.cs
+++ Assets/1.Scripts/CEA/MapEffectSpawner.cs
@@ -107,6 +107,7 @@ public class MapEffectSpawner : MonoBehaviour
                             break;
 
                     }
+                    break;
                 }
             }
         }
